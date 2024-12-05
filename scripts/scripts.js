f81b76ce23585b83fdfaf50f1d7859ad785fdfe6
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-05 16:00:51 -0600

    Complete RPS UI on new branch

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,29 @@
+/**********
+ * Rock Paper Scissors - UI addition
+ * 
+ * 
+ **********/
 
-function getHumanChoice() {
-    return prompt("What's your choice?").toLowerCase()
-}
+console.log(document.querySelectorAll('button'));
+
+const btn = Array.from(document.querySelectorAll('button'));
+
+const consoleDiv = document.querySelector('#console');
+
+const playerScoreDiv = document.querySelector('#playerScore');
+const computerScoreDiv = document.querySelector('#computerScore');
+
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    btn.forEach((element) => {
+        element.addEventListener('click', () => {
+            const computerChoice = getComputerChoice();
+            consoleDiv.textContent = `Player chose ${element.id}! | Computer chose `;
+            consoleDiv.textContent += `${computerChoice}!`;
+            return playRound(element.id, computerChoice);
+        })
+    })
+});
 
 function getComputerChoice() {
     const choice = Math.round(Math.random()) + Math.round(Math.random());
@@ -9,81 +31,96 @@ function getComputerChoice() {
 
     switch (choice) {
         case 0:
-            final = "rock";
+            final = 'rock';
             break;
         case 1:
-            final = "paper";
+            final = 'paper';
             break;
         case 2:
-            final = "scissors";
+            final = 'scissors';
             break;
         default:
-            final = "error"
+            final = 'error'
     }
     return final
 }
 
+let humanScore = 0;
+let computerScore = 0;
+let roundsPlayed = 0;
 
-function playGame(roundstoPlay) {
-    let humanScore = 0;
-    let computerScore = 0;
-    let roundsPlayed = 0;
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-
-    console.log(roundsPlayed, roundstoPlay);
-
-    function playRound(humanChoice, computerChoice) {
-        let combinedResult = humanChoice + "-" + computerChoice;
-        console.log(roundsPlayed);
-
-        switch (combinedResult) {
-            case "rock-rock":
-                console.log("Rock : Rock - Tie!");
-                break;
-            case "rock-paper":
-                console.log("Rock : Paper - Computer wins!");
-                computerScore++
-                break;
-            case "rock-scissors":
-                console.log("Rock : Scissors - Player wins!");
-                humanScore++
-                break;
-            case "paper-paper":
-                console.log("Paper : Paper - Tie!");
-                break;
-            case "paper-rock":
-                console.log("Paper : Rock - Player wins!");
-                humanScore++
-                break;
-            case "paper-scissors":
-                console.log("Paper : Scissors - Computer wins!");
-                computerScore++
-                break;
-            case "scissors-scissors":
-                console.log("Scissors : Scissors - Tie!");
-                break;
-            case "scissors-rock":
-                console.log("Scissors : Rock - Computer wins!");
-                computerScore++
-                break;
-            case "scissors-paper":
-                console.log("Scissors : Paper - Player wins!");
-                humanScore++
-                break;
-        }
-
-    }
-
-    while (roundsPlayed < roundstoPlay) {
-        console.log("if statement reached " + roundsPlayed);
-        roundsPlayed++;
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    return humanScore > computerScore ? console.log("Player wins!") : console.log("Computer wins!");
+resetGame = () => {
+    roundsPlayed = 0;
+    humanScore = 0;
+    computerScore = 0;
 }
 
-playGame(5);
+playRound = (humanChoice, computerChoice) => {
+    let combinedResult = humanChoice + '-' + computerChoice;
+    console.log(roundsPlayed);
+    roundsPlayed++;
+
+    evaluateRounds = () => {
+        if (roundsPlayed === 5) {
+            if (humanScore > computerScore) {
+                resetGame();
+                alert('Player wins!');
+            } else if (computerScore > humanScore) {
+                resetGame();
+                alert('Computer wins!');
+            }
+            resetGame();
+            alert("It's a tie!");
+        }
+    }
+    switch (combinedResult) {
+        case 'rock-rock':
+            console.log('Rock : Rock - Tie!');
+            evaluateRounds();
+            break;
+        case 'rock-paper':
+            console.log('Rock : Paper - Computer won round ' + roundsPlayed + ".");
+            computerScore++
+            computerScoreDiv.textContent = computerScore;
+            evaluateRounds();
+            break;
+        case 'rock-scissors':
+            console.log('Rock : Scissors - Player won round ' + roundsPlayed + ".");
+            humanScore++
+            playerScoreDiv.textContent = humanScore;
+            evaluateRounds();
+            break;
+        case 'paper-paper':
+            console.log('Paper : Paper - Tie!');
+            evaluateRounds();
+            break;
+        case 'paper-rock':
+            console.log('Paper : Rock - Player won round ' + roundsPlayed + ".");
+            humanScore++
+            playerScoreDiv.textContent = humanScore;
+            evaluateRounds();
+            break;
+        case 'paper-scissors':
+            console.log('Paper : Scissors - Computer won round ' + roundsPlayed + ".");
+            computerScore++
+            computerScoreDiv.textContent = computerScore;
+            evaluateRounds();
+            break;
+        case 'scissors-scissors':
+            console.log('Scissors : Scissors - Tie!');
+            break;
+        case 'scissors-rock':
+            console.log('Scissors : Rock - Computer won round ' + roundsPlayed + ".");
+            computerScore++
+            computerScoreDiv.textContent = computerScore;
+            evaluateRounds();
+            break;
+        case 'scissors-paper':
+            console.log('Scissors : Paper - Player won round ' + roundsPlayed + ".");
+            humanScore++
+            playerScoreDiv.textContent = humanScore;
+            evaluateRounds();
+            break;
+    }
+
+}
